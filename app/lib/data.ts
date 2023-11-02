5822e88367a432b496c7ce80b738ce09382d0b4e
Humberto Asca <hasca@ciplima.org.pe>
2023-11-02 18:46:11 -0500

    Dynamic rendering

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,3 +1,8 @@
+// Note: unstable_noStore is an experimental API and may change in the future. If you prefer to use a stable API in your own projects, you can also use the Segment Config Option export const dynamic = "force-dynamic".
+//This unstabke_noStore is used to make the page rendering dynamic and is experimental and will be changed in the future and is against the static rendering. Static rendering makes the page compiled at runtime, so the pages are preloaded fast. Dynamic rendering only gets data from the server per request.
+
+import { unstable_noStore as noStore } from 'next/cache';
+
 import { sql } from '@vercel/postgres';
 import {
   CustomerField,
@@ -12,18 +17,20 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
+  //This noStore() is experimental and will be changed in the future
+  noStore()
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
   try {
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch complete after 3 seconds.');
+    console.log('Data fetch complete after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -33,6 +40,10 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  // Add noStore() here prevent the response from being cached.
+  //This noStore() is experimental and will be changed in the future
+  noStore()
+  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -53,6 +64,11 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  // Add noStore() here prevent the response from being cached.
+  //This noStore() is experimental and will be changed in the future
+  noStore()
+  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -93,7 +109,10 @@ export async function fetchFilteredInvoices(
   currentPage: number,
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-
+  // Add noStore() here prevent the response from being cached.
+  //This noStore() is experimental and will be changed in the future
+  noStore()
+  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   try {
     const invoices = await sql<InvoicesTable>`
       SELECT
@@ -124,6 +143,10 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  // Add noStore() here prevent the response from being cached.
+  //This noStore() is experimental and will be changed in the future
+  noStore()
+  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -145,6 +168,10 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  // Add noStore() here prevent the response from being cached.
+  //This noStore() is experimental and will be changed in the future
+  noStore()
+  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -187,6 +214,10 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  // Add noStore() here prevent the response from being cached.
+  //This noStore() is experimental and will be changed in the future
+  noStore()
+  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   try {
     const data = await sql<CustomersTable>`
 		SELECT
