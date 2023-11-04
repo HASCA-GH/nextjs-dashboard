cd6f60a4680f3767d3b79c35f462f0f549003b98
Humberto Asca <hasca@ciplima.org.pe>
2023-11-04 12:23:43 -0500

    Mutating data

---
+++ app/lib/actions.ts
@@ -0,0 +1,79 @@
+'use server';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+// You'll notice that amount is of type string and not number. This is because input elements with type="number" actually return a string, not a number!
+
+// To handle type validation, you have a few options. While you can manually validate types, using a type validation library can save you time and effort. For your example, we'll use Zod, a TypeScript-first validation library that can simplify this task for you.
+
+// In your actions.ts file, import Zod and define a schema that matches the shape of your form object. This schema will validate the formData before saving it to a database.
+const InvoiceSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    // const rawFormData = {
+    //     customerId: formData.get('customerId'),
+    //     amount: formData.get('amount'),
+    //     status: formData.get('status'),
+    // };
+    // Test it out:
+    // console.log("Invoice data entered that was transformed as an object:", rawFormData);
+    // console.log("typeof rawFormData.amount:", typeof rawFormData.amount);
+
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0]; //let's create a new date with the format "YYYY-MM-DD" for the invoice's creation date
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+     `;
+
+    //Next.js has a Client-side Router Cache that stores the route segments in the user's browser for a time. Along with prefetching, this cache ensures that users can quickly navigate between routes while reducing the number of requests made to the server.
+
+    //  Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server. You can do this with the revalidatePath function from Next.js
+    revalidatePath('/dashboard/invoices');
+
+    //Once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server.
+    // At this point, you also want to redirect the user back to the /dashboard/invoices page. You can do this with the redirect function from Next.js
+    redirect('/dashboard/invoices');
+
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = InvoiceSchema.omit({ date: true, id: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse(
+        Object.fromEntries(formData.entries())
+    );
+
+    const amountInCents = amount * 100;
+
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+}
